136670bc373df0c9a7f33713e3b7e3c4b128b542
Mikhail Shapirov <shapirov@amazon.com>
2020-03-23 20:19:22 -0400

    minor changes:

--- argocd-add-apps.sh
+++ argocd-add-apps.sh
@@ -1,5 +1,2 @@
 argocd repo add git@github.com:shapirov103/argo-apps.git --ssh-private-key-path ~/.ssh/github_rsa --insecure-ignore-host-key
-argocd app create apps \
-    --dest-namespace argocd \
-    --dest-server https://kubernetes.default.svc \
-    --repo git@github.com:shapirov103/argo-apps.git 
+argocd app create apps --dest-namespace argocd  --dest-server https://kubernetes.default.svc  --repo git@github.com:shapirov103/argo-apps.git --path "."
